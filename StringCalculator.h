c30a39816c332d5d1e59d23c7efbdb4d70efe1a0
zopeharshal <harshalzope99@gmail.com>
2024-07-29 12:13:15 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -2,14 +2,11 @@
 #define STRINGCALCULATOR_H
 
 #include <string>
-#include <vector>
+#include <stdexcept>
 
 class StringCalculator {
 public:
- int add(const std::string& input);
-private:
- std::vector<int> parseNumbers(const std::string& input, const std::string& delimiter);
- void checkForNegatives(const std::vector<int>& numbers);
+    int add(const std::string& numbers);
 };
 
 #endif 
